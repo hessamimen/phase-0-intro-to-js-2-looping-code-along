e4f1ca73659ead33cc3e42922e23352d8a6566d3
Hessam Imen <hessam.imen@gmail.com>
2022-03-06 19:25:11 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+
+// for (let age = 30; age <40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     // debugger;
+// }
+
+// --------------------------------------------------------------//
+
+// const gifts = ['teady bear', 'drone', 'doll'];
+// function wrapGifts(gifts){
+//     for(let i=0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+// --------------------------------------------------------------//
+
+// function writeCards(names, event){
+//     for(i=0; i < names.length; i++){
+//         console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+//         debugger;
+//     }
+// }
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+// --------------------------------------------------------------//
+
+// function countDown(num){
+//     let i = num;
+//     while(i >= 0 ){
+//     console.log(i);
+//     i--;
+// }
+// }
+// countDown(10);
+
+// --------------------------------------------------------------//
